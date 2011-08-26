d242f232847a82a36e9b8256ed82314af01376bc
Daniel Mircea <daniel@thegeek.ro>
2011-08-26 12:23:49 +0300

    Moved sitemap into a module.

--- lib/sitemap.rb
+++ lib/sitemap.rb
@@ -10,58 +10,62 @@ gem "builder", "~> 2.1.2"
 require "singleton"
 require "builder"
 
-class Sitemap
+module Sitemap
 
-  include Singleton
-  include Rails.application.routes.url_helpers
+  class Generator
 
-  VERSION = Gem::Specification.load(File.expand_path("../sitemap.gemspec", File.dirname(__FILE__))).version.to_s
+    include Singleton
+    include Rails.application.routes.url_helpers
 
-  attr_accessor :entries, :host
+    VERSION = Gem::Specification.load(File.expand_path("../sitemap.gemspec", File.dirname(__FILE__))).version.to_s
 
-  def initialize
-    self.entries = []
-  end
+    attr_accessor :entries, :host
 
-  def render(options = {}, &block)
-    options.each do |k, v|
-      self.send("#{k}=", v)
+    def initialize
+      self.entries = []
     end
-    instance_exec(self, &block)
-  end
 
-  def path(object, options = {})
-    self.entries << {
-      :object => object,
-      :options => options.reject { |k, v| k == :host },
-      :host => options[:host]
-    }
-  end
-
-  def collection(type, options = {})
-    objects = options[:objects] ? options[:objects].call : type.to_s.classify.constantize.all
-    objects.each do |object|
-      path(object, options.reject { |k, v| k == :objects })
+    def render(options = {}, &block)
+      options.each do |k, v|
+        self.send("#{k}=", v)
+      end
+      instance_exec(self, &block)
     end
-  end
 
-  def build
-    xml = Builder::XmlMarkup.new(:indent => 2)
-    file = File.read(File.expand_path("../views/index.xml.builder", __FILE__))
-    instance_eval file
-  end
+    def path(object, options = {})
+      self.entries << {
+        :object => object,
+        :options => options.reject { |k, v| k == :host },
+        :host => options[:host]
+      }
+    end
 
-  def save(location)
-    file = File.new(location, "w")
-    file.write(build)
-    file.close
-  end
+    def collection(type, options = {})
+      objects = options[:objects] ? options[:objects].call : type.to_s.classify.constantize.all
+      objects.each do |object|
+        path(object, options.reject { |k, v| k == :objects })
+      end
+    end
 
-  def get_url(entry)
-    options = {
-      :host => entry[:host] ? entry[:host].call(entry[:object]) : host
-    }
-    polymorphic_url(entry[:object], options)
+    def build
+      xml = Builder::XmlMarkup.new(:indent => 2)
+      file = File.read(File.expand_path("../views/index.xml.builder", __FILE__))
+      instance_eval file
+    end
+
+    def save(location)
+      file = File.new(location, "w")
+      file.write(build)
+      file.close
+    end
+
+    def get_url(entry)
+      options = {
+        :host => entry[:host] ? entry[:host].call(entry[:object]) : host
+      }
+      polymorphic_url(entry[:object], options)
+    end
+
   end
 
 end
